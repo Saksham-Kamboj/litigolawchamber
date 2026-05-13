70f9bda75945235445ac4526a0813b51fcb08381
Saksham Kamboj <saksham@ekvayu.com>
2026-05-13 10:31:34 +0530

    update

--- src/pages/KnowledgeGateway.jsx
+++ src/pages/KnowledgeGateway.jsx
@@ -1,9 +1,16 @@
 import ArticleSection from "../components/KnowledgeGateway/ArticleSection";
 import NewsSection from "../components/KnowledgeGateway/NewsSection";
+import PageHeroSection from "../components/PageHeroSection";
+import KnowledgeGatewayHeroImage from "../assets/knowledge-gateway.avif";
 
 export default function KnowledgeGateway() {
   return (
     <div>
+      <PageHeroSection
+        image={KnowledgeGatewayHeroImage}
+        title="Knowledge Gateway"
+        subtitle="Stay updated with the latest legal insights"
+      />
       <ArticleSection />
       <NewsSection />
     </div>
